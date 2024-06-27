0e173becfe67e34c91d600a19d9743c68a4f3af0
Vishnu Bovilla <vbovilla@dminc.com>
2024-06-26 22:47:14 -0400

    table generator strategy changes.

--- src/main/java/in/dminc/models/Author.java
+++ src/main/java/in/dminc/models/Author.java
@@ -11,15 +11,27 @@ import lombok.NoArgsConstructor;
 public class Author {
 
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "author_sequence"
-    )
-    @SequenceGenerator(
-            name = "author_sequence",
-            sequenceName = "author_sequence",
-            allocationSize = 1
-    )
+//    @GeneratedValue(
+//            strategy = GenerationType.SEQUENCE,
+//            generator = "author_sequence"
+//    )
+//    @SequenceGenerator(
+//            name = "author_sequence", // name should be same as on the 'generator' in @GeneratedValue.
+//            sequenceName = "author_sequence",
+//            allocationSize = 1
+//    )
+
+//    @GeneratedValue(
+//            strategy = GenerationType.TABLE,
+//            generator = "author_id_gen"
+//    )
+//    @TableGenerator(
+//            name = "author_id_gen",  // name should be same as on the 'generator' in @GeneratedValue.
+//            table = "author_id_generator",
+//            pkColumnName = "id_name",
+//            valueColumnName = "id_value",
+//            allocationSize = 1
+//    )
     private Integer id;
     private String firstName;
     private String lastName;
